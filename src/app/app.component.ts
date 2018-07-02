9d648c091d6b00b305376b03db3849f3f42f9b76
gaglage <gabialvarez@gmail.com>
2018-07-02 21:11:31 +0200

    import libraries

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,15 @@
 import { Component } from '@angular/core';
 
+
+import 'core-js';
+import RainRenderer from "./shared/rain-rendere.jsr";
+import Raindrops from "./shared/raindrops.js";
+import loadImages from "./shared/image-loader.js";
+import createCanvas from "./shared/create-canvas.js";
+import TweenLite from 'gsap.js';
+import times from './shared/times.js';
+import {random,chance} from './shared/random.js';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
